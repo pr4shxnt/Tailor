eff1b5e93caab4e3869694e98f7258d3a31103f2
Prashant Adhikari <prashantadhikareey@gmail.com>
2024-11-28 22:29:28 +0545

    Footer update

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+
+const Footer = () => {
+    return (
+      <footer>
+        <section className="bg-black h-full rounded-t-lg text-white w-full flex justify-between px-64">
+            <div className="left">
+<h1>HELLO WORLD</h1>
+            </div>
+            <div className="right">
+                <h1>Bye world</h1>
+            </div>
+        </section>
+      </footer>
+    );
+  };
+  
+  export default Footer;
