7011a5e98b7039981384d993d675b0af6feb28f9
Prashant Adhikari <130303397+pr4shxnt@users.noreply.github.com>
2024-12-28 20:19:52 +0545

    Update ScrollToTop.jsx

--- essUtils/ScrollToTop.jsx
+++ essUtils/ScrollToTop.jsx
@@ -4,6 +4,8 @@ import { useLocation } from "react-router-dom";
 const ScrollToTop = () => {
   const { pathname } = useLocation();
 
+
+  
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
