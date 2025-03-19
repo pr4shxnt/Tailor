40c8aba61d85e6ba574dc37ab54e444f88103922
Prashant Adhikari <prashantadhikareeey@gmail.com>
2025-03-19 19:53:28 +0545

    w-40 to w-full

--- src/Components/UserAccount/UserDetailsMeasurement.jsx
+++ src/Components/UserAccount/UserDetailsMeasurement.jsx
@@ -5,7 +5,7 @@ import Measurement from './Measurement'
 const UserDetailsMeasurement = () => {
   return (
     <div className='flex flex-col md:flex-row w-full   min-h-full bg-gradient-to-b from-gray-300 to-gray-100 p-4'>
-      <div className="w-[40%]"><UserDetails/></div>
+       <UserDetails/> 
         
         <Measurement/>
     </div>
