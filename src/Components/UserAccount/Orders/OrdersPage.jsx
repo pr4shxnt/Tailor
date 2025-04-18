d5b5c358440800cc5d826cd8dcbc0ed632f3de8e
Prashant Adhikari <prashantadhikareeey@gmail.com>
2025-04-18 20:56:00 +0545

    order page singareko

--- src/Components/UserAccount/Orders/OrdersPage.jsx
+++ src/Components/UserAccount/Orders/OrdersPage.jsx
@@ -1,44 +1,60 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Context } from '../../../Context/ContextProvider';
 import OrderTracking from './OrderTracking';
 import { NavLink } from 'react-router-dom';
+import { Settings } from 'lucide-react';
 
 const OrdersPage = () => {
   const { orderContainer } = useContext(Context);
+
+  const [theme, setTheme] = useState('') 
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme')  
+    setTheme(storedTheme);
+   }, [theme])
+  
 
   console.log("Order Container:", orderContainer);
 
   return (
     <div>
       <div className='flex flex-col gap-4'>
-        <h1 className='text-2xl font-bold'>My Orders</h1>
-        <div className='flex flex-col gap-4'>
+         <div className='flex flex-col gap-5'>
           {orderContainer?.length === 0 ? (
             <p className='text-center text-gray-500'>No orders found.</p>
           ) : (
             orderContainer.map((order, index) => (
-              <div key={index} className='border p-4 rounded-md'>
-                <h2 className='text-lg font-semibold'>
-                  Order ID: {order.transaction_uuid?.split('-').slice(2).join('-')}
+              <div key={index} className='shadow-lg shadow-tertiary p-4  '>
+                <h2 className='tracking-wider uppercase flex  '>
+                  <h1 className="font-light">Order Number</h1> : {order.transaction_uuid?.split('-').slice(2).join('-')}
                 </h2>
-                <p>Items:</p>
+                <div className="mx-5 p-3 my-3 bg-primary text-tertiary rounded">
                 <div className='flex flex-col gap-2 pl-5'>
                   {order.Products?.map((item, idx) => (
-                    <div key={idx} className='flex items-center gap-2'>
+                    <div key={idx} title={`Click to view ${item.id.name}'s page`} className='flex tracking-wider uppercase font-light hover:scale-105 cursor-pointer transition-all duration-700 items-center gap-2'>
                       <img
                         src={item.id.images[0]}
-                        alt={item.id.name}
-                        className='h-8 w-8 object-cover rounded'
+                        alt={item.id.name.slice(0, 8)}
+                        className='h-10 w-10 object-cover shadow-md'
                       />
-                      <span>{item.id.name} : Npr. {item.id.price} x {item.quantity} (Npr. {item.price})</span>
+                      <span>{item.id.name.slice(0,10)+"..."}  x {item.quantity} </span>
                     </div>
                   ))}
                 </div>
-                <p className='mt-2'>Total Amount: ${order.totalPrice}</p>
-                <p>Status: {order.status || "Pending"}</p>
-                <button className="">
-                  <NavLink to={`/user/order-track/${order._id}`}>Check</NavLink>
+                <p className='mt-2 text-end  px-5'>Total Amount: Npr. {order.totalPrice}</p>
+                </div>
+                <div className="flex items-center justify-between mt-2">
+                <div className="">
+                <p className='flex gap-1 font-light items-center uppercase tracking-wider'> <Settings color='gold'/> Order {order?.status}</p></div>
+                <div className="flex gap-2">
+                <button className="py-2 px-3 bg-purple-500 text-primary  hover:bg-secondary transition duration-200">
+                  <NavLink to={`/user/order/invoice/${order._id}`}>See Invoice</NavLink>
                 </button>
+                <button className="py-2 px-3 bg-tertiary text-primary  hover:bg-secondary transition duration-200">
+                  <NavLink to={`/user/order-track/${order._id}`}>Track Order</NavLink>
+                </button></div>
+                </div>
               </div>
             ))
           )}
